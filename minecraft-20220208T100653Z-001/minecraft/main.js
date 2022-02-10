adc9f83005efed3b156e20bf2b0539d273d3ae3d
Aayushthe <86201033+Aayushthe@users.noreply.github.com>
2022-02-10 15:38:32 +0530

    Add files via upload

--- minecraft-20220208T100653Z-001/minecraft/main.js
+++ minecraft-20220208T100653Z-001/minecraft/main.js
@@ -1,12 +1,12 @@
 var canvas = new fabric.Canvas("myCanvas");
 player_x= 10;
 player_y = 10;
-block_img_width= 30;
+block_image_width= 30;
 block_img_height = 30;
 var player_object="";
 var block_img_object="";
 function player_update(){
-    fabric.Image.formURL("player.png", function(Img){
+    fabric.Image.fromURL("player.png", function(Img){
         player_object= Img;
         player_object.scaleToWidth(150);
         player_object.scaleToHeight(140);
@@ -19,11 +19,11 @@ function player_update(){
     })
 }
  function new_image(get_image){
-    fabric.Image.formURL(get_image, function(Img){
-        block_image_object= Img;
-        block_image_object.scaleToWidth( block_image_width);
-        block_image_object.scaleToHeight( block_image_height);
-        block_image_object.set({
+    fabric.Image.fromURL(get_image, function(Img){
+        block_img_object= Img;
+        block_img_object.scaleToWidth( block_image_width);
+        block_img_object.scaleToHeight( block_img_height);
+        block_img_object.set({
         top:player_y,
         left:player_x
         });
@@ -32,20 +32,20 @@ function player_update(){
  }
  window.addEventListener("keydown", my_keydown);
  function my_keydown(e){
-     keyPressed = e.keypressed;
+     keyPressed = e.KeyCode;
      console.log(keyPressed);
      if(e.shiftkey == true && keypressed == "80"){
          console.log("p and shift pressed together");
          block_image_width = block_image_width + 10;
-         block_image_height = block_image_height + 10;
+         block_img_height = block_img_height + 10;
          document.getElementById("current_width").innerHTML = block_image_width;
-         document.getElementById("current_height").innerHTML = block_image_height;
+         document.getElementById("current_height").innerHTML = block_img_height;
      }
      if (e.shiftkey && keypressed == "77"){
         block_image_width = block_image_width - 10;
-        block_image_height = block_image_height - 10;
+        block_img_height = block_img_height - 10;
         document.getElementById("current_width").innerHTML = block_image_width;
-        document.getElementById("current_height").innerHTML = block_image_height;
+        document.getElementById("current_height").innerHTML = block_img_height;
      }
      if(keyPressed == "38"){
          up()
@@ -106,7 +106,7 @@ function player_update(){
  }
  function down(){
      if(player_y <= 500){
-         player_y = player_y + block_image_height
+         player_y = player_y + block_img_height
          console.log("block image width = " + block_image_width)
          console.log("When Left arrow key is pressed, X =" + player_x + " , y = " +player_y)
          canvas.remove(player_object);
@@ -122,7 +122,7 @@ function left(){
 }
 function right(){
     if(player_x >0){
-        player_x = player_x - block_image_width
+        player_x = player_x + block_image_width
         canvas.remove(player_object)
         player_update()
     }
